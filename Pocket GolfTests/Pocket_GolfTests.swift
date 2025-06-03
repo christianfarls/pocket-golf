5733431408e2930a472e1681ff525ed3ee8ad26f
Christian Farls <cfarls@nd.edu>
2025-06-03 19:30:22 -0400

    Initial Commit

---
+++ Pocket GolfTests/Pocket_GolfTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_GolfTests.swift
+//  Pocket GolfTests
+//
+//  Created by Christian Farls on 6/3/25.
+//
+
+import Testing
+@testable import Pocket_Golf
+
+struct Pocket_GolfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Pocket GolfUITests/Pocket_GolfUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Pocket_GolfUITestsLaunchTests.swift
+//  Pocket GolfUITests
+//
+//  Created by Christian Farls on 6/3/25.
+//
+
+import XCTest
+
+final class Pocket_GolfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
